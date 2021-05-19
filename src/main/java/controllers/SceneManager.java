eed8763abc0cd423aa4f9a1846dbd4a39779ab38
Jakob <jakoblundbergsu15@gmail.com>
2021-05-19 15:28:03 +0200

    La till en SceneManager
    Signed-off-by: Jakob <jakoblundbergsu15@gmail.com>

---
+++ src/main/java/controllers/SceneManager.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.scene.Scene;
+
+public class SceneManager
+{
+
+    private final Scene scene;
+
+    public SceneManager(Scene scene) {this.scene = scene;}
+
+}
