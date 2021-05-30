2b883a6d301d755acbf982b84576300de95e451e
Jakob <jakoblundbergsu15@gmail.com>
2021-05-30 14:59:09 +0200

    Tried doing ListViews, trying TableViews
    Signed-off-by: Jakob <jakoblundbergsu15@gmail.com>

--- src/main/java/MonkeLogic/methods/ShowAccountsAdminFrontEnd.java
+++ src/main/java/MonkeLogic/methods/ShowAccountsAdminFrontEnd.java
@@ -1,4 +1,45 @@
 package MonkeLogic.methods;
 
-public class ShowAccountsAdminFrontEnd {
+import MonkeLogic.controllers.SceneManager;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShowAccountsAdminFrontEnd implements Initializable {
+
+    //region This is the Lists Variables
+    @FXML
+    private HBox listsHBox;
+    @FXML
+    private final ListView<String> websites = new ListView<>();
+    @FXML
+    private final ListView<String> users = new ListView<>();
+    @FXML
+    private final ListView<String> usernames = new ListView<>();
+    //endregion
+
+    //region
+    @FXML
+    private TableView accounts;
+
+    //endregion
+
+    //region This happens on init
+    private SceneManager sceneManager;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        sceneManager = SceneManager.getInstance();
+    }
+    //endregion
+
+    private void loadAccounts() {
+
+
+    }
 }
