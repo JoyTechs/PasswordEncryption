697edc6bf2e08d710a98e4f38c0c9cde00900199
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-05-31 10:28:00 +0200

    slight edit to memes directory

--- src/main/java/MonkeLogic/backEnd/LoginBackEnd.java
+++ src/main/java/MonkeLogic/backEnd/LoginBackEnd.java
@@ -5,14 +5,14 @@ import MonkeLogic.controllers.SceneManager;
 public class LoginBackEnd {
 
     private final SceneManager sceneManager;
-//    private final String activeUsersID;
-//    private final String activeUsersClearanceLevel;
+    private final String activeUsersID;
+    private final String activeUsersClearanceLevel;
 
     public LoginBackEnd(String username, String password) {
         sceneManager = SceneManager.getInstance();
-//        activeUsersID = username;
-//        activeUsersClearanceLevel = password;
-//        sceneManager.memes();
+        activeUsersID = username;
+        activeUsersClearanceLevel = password;
+        sceneManager.memes();
         sceneManager.createAccount();
 
     }
